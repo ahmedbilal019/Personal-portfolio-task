f5056d6458d92cb8f79868636430c737d66dba3d
Ahmed Bilal <ahmed258bilal@gmail.com>
2026-08-22 04:04:07 +0500

    creating list for navbar section and add component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
+import Navbar from "./Components/Navbar";
 // import "./App.css";
 
 function App() {
   return (
     <>
       <h1>My personal project</h1>
+      <Navbar></Navbar>
     </>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,1 +1,15 @@
+import React from "react";
 
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
