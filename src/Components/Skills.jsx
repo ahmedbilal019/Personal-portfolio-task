cfb38e3bc5ee4b1fc8b009fdb726044fd899aa15
Ahmed Bilal <ahmed258bilal@gmail.com>
2026-08-23 03:06:09 +0500

    displaying skills icon using .map function

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import { FaHtml5 } from "react-icons/fa6";
+import { FaCss3Alt } from "react-icons/fa";
+import { FaJs } from "react-icons/fa6";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { RiReactjsFill } from "react-icons/ri";
 function Skills() {
+  const skills = [
+    { name: "HTML", icon: <FaHtml5 /> },
+    { name: "CSS", icon: <FaCss3Alt /> },
+    { name: "Javascript", icon: <FaJs /> },
+    { name: "Tailwindcss", icon: <RiTailwindCssFill /> },
+    { name: "Reactjs", icon: <RiReactjsFill /> },
+  ];
+
   return (
     <div>
-      <h1>Skills</h1>
-      <FaHtml5 />
+      {skills.map((skill) => (
+        <div key={skill.name}>
+          {skill.icon}
+          <span>{skill.name}</span>
+        </div>
+      ))}
     </div>
   );
 }
