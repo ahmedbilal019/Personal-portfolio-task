87cdd3fb595c802f5ec8a639d1ec1cd6f7e77ade
Ahmed Bilal <ahmed258bilal@gmail.com>
2026-08-23 23:41:17 +0500

    applying style and adjust the locagtion of skilss card

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -4,23 +4,34 @@ import { FaCss3Alt } from "react-icons/fa";
 import { FaJs } from "react-icons/fa6";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { RiReactjsFill } from "react-icons/ri";
+
 function Skills() {
   const skills = [
-    { name: "HTML", icon: <FaHtml5 /> },
-    { name: "CSS", icon: <FaCss3Alt /> },
-    { name: "Javascript", icon: <FaJs /> },
-    { name: "Tailwindcss", icon: <RiTailwindCssFill /> },
-    { name: "Reactjs", icon: <RiReactjsFill /> },
+    { name: "HTML", icon: FaHtml5 },
+    { name: "CSS", icon: FaCss3Alt },
+    { name: "Javascript", icon: FaJs },
+    { name: "Tailwindcss", icon: RiTailwindCssFill },
+    { name: "Reactjs", icon: RiReactjsFill },
   ];
 
   return (
-    <div>
-      {skills.map((skill) => (
-        <div key={skill.name}>
-          {skill.icon}
-          <span>{skill.name}</span>
-        </div>
-      ))}
+    <div className="skillscard flex flex-row justify-evenly">
+      {skills.map((skill) => {
+        const Icon = skill.icon;
+
+        return (
+          <div
+            key={skill.name}
+            className="flex flex-col items-center gap-2"
+          >
+            <Icon size={50} />
+
+            <span className="text-2xl">
+              {skill.name}
+            </span>
+          </div>
+        );
+      })}
     </div>
   );
 }
