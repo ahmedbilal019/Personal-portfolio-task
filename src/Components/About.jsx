831c4da16af948da86b15b5809fc269694d43bad
Ahmed Bilal <ahmed258bilal@gmail.com>
2026-09-12 12:34:32 +0500

    making section responsive

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -13,22 +13,7 @@ function About() {
           alt="my profile image "
         />
       </div>
-      {/* <div className="aboutMe">
-        <h2 className="text-2xl text-orange-500 my-2">Who i am?</h2>
-        <p className="w-2xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
-          dignissimos ad ipsum officia beatae numquam soluta sunt sint odio quod
-          debitis voluptatum aliquam, expedita cupiditate dolor quos aliquid
-          natus pariatur.
-        </p>
-        <br />
-        <p className="w-2xl">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error veniam
-          ducimus ipsum aliquam! Dolorum dolor reiciendis recusandae rem?
-          Doloremque, praesentium sit? Esse, quae. Harum quis voluptatem
-          voluptatum neque voluptas quam.
-        </p>
-      </div> */}
+      
       <div className="aboutMe w-full max-w-3xl px-4 sm:px-6 md:px-0">
         <h2 className="text-2xl text-orange-500 my-2">About Me!</h2>
 
